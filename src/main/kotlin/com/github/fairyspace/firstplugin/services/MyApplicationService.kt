6c4126913b475e0e47e06715d64ae536e06e7b67
GitHub Action <action@github.com>
2023-03-04 11:15:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/fairyspace/firstplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/fairyspace/firstplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.fairyspace.firstplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fairyspace.firstplugin.MyBundle
 
 class MyApplicationService {
 
